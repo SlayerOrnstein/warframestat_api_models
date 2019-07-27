3b8e1cf33bc4f4aa689599dd1b86e0e0e55877e3
Ludwig <blacklightinglr@gmail.com>
2019-07-27 16:59:55 -0400

    yeah no generated files should be tracked

---
+++ lib/models/syndicate.g.dart
@@ -0,0 +1,51 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'syndicate.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Syndicate _$SyndicateFromJson(Map<String, dynamic> json) {
+  return Syndicate(
+    id: json['id'] as String,
+    activation: json['activation'] == null
+        ? null
+        : DateTime.parse(json['activation'] as String),
+    expiry: json['expiry'] == null
+        ? null
+        : DateTime.parse(json['expiry'] as String),
+    name: json['syndicate'] as String,
+    active: json['active'] as bool,
+    jobs: (json['jobs'] as List)
+        ?.map(
+            (e) => e == null ? null : Jobs.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+  );
+}
+
+Map<String, dynamic> _$SyndicateToJson(Syndicate instance) => <String, dynamic>{
+      'id': instance.id,
+      'activation': instance.activation?.toIso8601String(),
+      'expiry': instance.expiry?.toIso8601String(),
+      'syndicate': instance.name,
+      'active': instance.active,
+      'jobs': instance.jobs,
+    };
+
+Jobs _$JobsFromJson(Map<String, dynamic> json) {
+  return Jobs(
+    type: json['type'] as String,
+    pool: json['rewardPool'],
+    enemyLevels: (json['enemyLevels'] as List)?.map((e) => e as int)?.toList(),
+    standingStages:
+        (json['standingStages'] as List)?.map((e) => e as int)?.toList(),
+  );
+}
+
+Map<String, dynamic> _$JobsToJson(Jobs instance) => <String, dynamic>{
+      'type': instance.type,
+      'rewardPool': instance.pool,
+      'enemyLevels': instance.enemyLevels,
+      'standingStages': instance.standingStages,
+    };
---
+++ lib/models/worldstate.g.dart
@@ -0,0 +1,83 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'worldstate.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Worldstate _$WorldstateFromJson(Map<String, dynamic> json) {
+  return Worldstate(
+    timestamp: json['timestamp'] as String,
+    news: (json['news'] as List)
+        ?.map((e) =>
+            e == null ? null : OrbiterNews.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    events: (json['events'] as List)
+        ?.map(
+            (e) => e == null ? null : Event.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    alerts: (json['alerts'] as List)
+        ?.map(
+            (e) => e == null ? null : Alert.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    sortie: json['sortie'] == null
+        ? null
+        : Sortie.fromJson(json['sortie'] as Map<String, dynamic>),
+    syndicateMissions: (json['syndicateMissions'] as List)
+        ?.map((e) =>
+            e == null ? null : Syndicate.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    fissures: (json['fissures'] as List)
+        ?.map((e) =>
+            e == null ? null : VoidFissure.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    invasions: (json['invasions'] as List)
+        ?.map((e) =>
+            e == null ? null : Invasion.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    voidTrader: json['voidTrader'] == null
+        ? null
+        : VoidTrader.fromJson(json['voidTrader'] as Map<String, dynamic>),
+    dailyDeals: (json['dailyDeals'] as List)
+        ?.map((e) =>
+            e == null ? null : DarvoDeal.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    persistentEnemies: (json['persistentEnemies'] as List)
+        ?.map((e) => e == null
+            ? null
+            : PersistentEnemies.fromJson(e as Map<String, dynamic>))
+        ?.toList(),
+    earthCycle: json['earthCycle'] == null
+        ? null
+        : Earth.fromJson(json['earthCycle'] as Map<String, dynamic>),
+    cetusCycle: json['cetusCycle'] == null
+        ? null
+        : Earth.fromJson(json['cetusCycle'] as Map<String, dynamic>),
+    vallisCycle: json['vallisCycle'] == null
+        ? null
+        : Vallis.fromJson(json['vallisCycle'] as Map<String, dynamic>),
+    nightwave: json['nightwave'] == null
+        ? null
+        : Nightwave.fromJson(json['nightwave'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$WorldstateToJson(Worldstate instance) =>
+    <String, dynamic>{
+      'timestamp': instance.timestamp,
+      'news': instance.news,
+      'events': instance.events,
+      'alerts': instance.alerts,
+      'sortie': instance.sortie,
+      'syndicateMissions': instance.syndicateMissions,
+      'fissures': instance.fissures,
+      'invasions': instance.invasions,
+      'voidTrader': instance.voidTrader,
+      'dailyDeals': instance.dailyDeals,
+      'persistentEnemies': instance.persistentEnemies,
+      'earthCycle': instance.earthCycle,
+      'cetusCycle': instance.cetusCycle,
+      'vallisCycle': instance.vallisCycle,
+      'nightwave': instance.nightwave,
+    };
