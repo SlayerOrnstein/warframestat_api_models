3cc8c14de663fc946194a07b96ad7fdc3ca0e150
Ludwig <blacklightinglr@gmail.com>
2019-08-02 07:31:46 -0400

    update README and example

--- example/worldstateapi_example.dart
+++ example/worldstateapi_example.dart
@@ -4,10 +4,10 @@ import 'package:worldstate_model/worldstate_models.dart';
 import 'package:http/http.dart' as http;
 
 Future<void> main() async {
-  final response =
-      json.decode((await http.get('https://api.warframestat.us/pc')).body);
+  final response = await http.get('https://api.warframestat.us/pc');
+  final worldstateJson = json.decode(response.body);
 
-  final worldstate = Worldstate.fromJson(response);
+  final worldstate = Worldstate.fromJson(worldstateJson);
 
   print(worldstate.timestamp); //prints the generated timestamp from worldstate
 }
