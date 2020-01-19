d0c362330dcab87be3e086f13b279d67da44df35
SlayerOrnstein <blacklightinglr@gmail.com>
2020-01-18 22:14:59 -0500

    export SlimDrop model

--- lib/misc.dart
+++ lib/misc.dart
@@ -1,4 +1,5 @@
 export 'src/models/misc/riven_data.dart';
 export 'src/models/misc/riven_roll.dart';
+export 'src/models/misc/slim_drop.dart';
 export 'src/models/misc/synth_location.dart';
 export 'src/models/misc/synth_target.dart';
