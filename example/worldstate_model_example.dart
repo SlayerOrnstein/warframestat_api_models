d68d1e568f49d8f437727a35c1d74d48e7b0899a
SlayerOrnstein <blacklightinglr@gmail.com>
2020-06-01 17:00:38 -0400

    tweaked package name a bit

--- example/worldstate_model_example.dart
+++ example/worldstate_model_example.dart
@@ -1,8 +1,7 @@
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-import 'package:warframestat_api_model/entities.dart';
-import 'package:warframestat_api_model/models.dart';
+import 'package:warframestat_api_models/warframestat_api_models.dart';
 
 Future<void> main() async {
   final data = await http.get('https://api.warframestat.us/pc');
--- test/worldstate_model_test.dart
+++ test/worldstate_model_test.dart
@@ -2,8 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:test/test.dart';
-import 'package:warframestat_api_model/entities.dart';
-import 'package:warframestat_api_model/models.dart';
+import 'package:warframestat_api_models/warframestat_api_models.dart';
 
 Future<void> main() async {
   final file = await File('test/worldstate.json').readAsString();
